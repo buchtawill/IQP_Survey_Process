6fee3a0a3e8438f4011b85a3d72964ca36cce0bc
buchtawill <buchtawill@gmail.com>
2024-04-21 15:01:03 +0800

    Plot other by dict

--- process_survey.py
+++ process_survey.py
@@ -4,6 +4,8 @@ from matplotlib import image            # type: ignore
 from matplotlib import pyplot as plt    # type: ignore
 import pandas as pd                     # type: ignore
 import numpy as np                      # type: ignore
+import os
+import shutil
 
 '''
 
@@ -97,27 +99,27 @@ survey = [
 ]
 
 response_options = {
-'country_options' :     ['United States', 'Taiwan', 'Other'],
-'resident_options' :    ['Yes', 'No'],
-'age_options' :         ['18-29', '30-44', '45-59', '60+'],
-'gender_options' :      ['Male', 'Female', 'Do not wish to say', 'other'],
-'view_options' :        ['Smartphone', 'Tablet', 'Desktop / Laptop'],
+'country' :                 ['United States', 'Taiwan', 'Other'],
+'local resident' :          ['Yes', 'No'],
+'age' :                     ['18-29', '30-44', '45-59', '60+'],
+'gender' :                  ['Male', 'Female', 'Do not wish to say', 'other'],
+'view mode' :               ['Smartphone', 'Tablet', 'Desktop / Laptop'],
 
-'experience_options' :  ['Very Poor', 'Poor', 'Neutral', 'Good',  'Very Good'],
-'navigation_options' :  ['Very Difficult', 'Difficult', 'Neutral', 'Easy', 'Very Easy'],
-'present_options' :     ['Stronly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree'],
-'speed_options' :       ['Very Dissatisfied', 'Dissatisfied', 'Neutral', 'Satisfied', 'Very Satisfied'],
+'overall experience' :      ['Very Poor', 'Poor', 'Neutral', 'Good',  'Very Good'],
+'ease of navigation' :      ['Very Difficult', 'Difficult', 'Neutral', 'Easy', 'Very Easy'],
+'enjoyed presentation' :    ['Stronly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree'],
+'load speed' :              ['Very Dissatisfied', 'Dissatisfied', 'Neutral', 'Satisfied', 'Very Satisfied'],
 
-'friend_options' :      ['Very Unlikely', 'Unlikely', 'Neutral', 'Likely', 'Very Likely'],
-'culture_options' :     ['Stronly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree'],
-'visiting_options' :    ['Very Unlikely', 'Unlikely', 'Neutral', 'Likely', 'Very Likely'],
-'modernize_options' :   ['Stronly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree'],
-'impact_options'   :    ['None', '---', '---', '---', 'Very Impactful'],
+'recommend to friend' :     ['Very Unlikely', 'Unlikely', 'Neutral', 'Likely', 'Very Likely'],
+'captures culture' :        ['Stronly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree'],
+'use when visiting' :       ['Very Unlikely', 'Unlikely', 'Neutral', 'Likely', 'Very Likely'],
+'showcases modernization' : ['Stronly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree'],
+'impactfulness'   :         ['None', '---', '---', '---', 'Very Impactful'],
 
-'fav_story_options' :   ['Wu, Jian-Hong (吳儉鴻)', 'Wang, Chun-Kai (王俊凱)', 'Lily (莉莉)'],
-'fav_story_reason' :    [],
-'fav_place_options' :   ['Zhishanyan Huiji Temple (芝山巖惠濟宮)', 'Taipei MRT (台北捷運)', 'Shilin Elementary School (士林國小)', 'Shilin Paper Mill (士林紙廠)', 'Shilin Architecture (士林建築)'],
-'fav_place_reason' :    []
+'favorite story' :          ['Wu, Jian-Hong (吳儉鴻)', 'Wang, Chun-Kai (王俊凱)', 'Lily (莉莉)'],
+'favorite place' :          ['Zhishanyan Huiji Temple (芝山巖惠濟宮)', 'Taipei MRT (台北捷運)', 
+                             'Shilin Elementary School (士林國小)', 'Shilin Paper Mill (士林紙廠)', 
+                             'Shilin Architecture (士林建築)'],
 }
 
 
@@ -174,6 +176,7 @@ IMAGE_TRANSPARENCY = False
 IMAGE_TEXT_COLOR = 'black'
 IMAGE_AXIS_COLOR = 'black'
 OUTPUT_IMAGE_LOCATION = "./out/"
+SAVE = True
 
 def setMatplotParams():
     matplotlib.rcParams['text.color'] = IMAGE_TEXT_COLOR
@@ -182,36 +185,94 @@ def setMatplotParams():
     matplotlib.rcParams['ytick.color'] = IMAGE_TEXT_COLOR
     matplotlib.rcParams['axes.edgecolor'] = IMAGE_AXIS_COLOR
 
-def plot_country(df: pd.DataFrame):
-    bar_graph_ax = data['country'].value_counts(sort=True).plot(kind='bar')
+def plot_country(df: pd.DataFrame, title: str):
+    bar_graph_ax = df['country'].value_counts(sort=True).plot(kind='bar')
     plt.bar_label(bar_graph_ax.containers[0])
     plt.xticks(rotation=0, ha='center')
-    plt.title(f"Country of Residence (n={len(data['country'])})")
+    plt.title(f"{title} (n={len(df['country'])})")
     plt.ylabel("Count")
     plt.xlabel("Country")
     #plt.savefig(OUTPUT_IMAGE_LOCATION + 'ages.png', transparent=IMAGE_TRANSPARENCY, bbox_inches='tight')
     plt.show()
 
-
-def plot_viewmode(df: pd.DataFrame):
-    bar_graph_ax = data['view mode'].value_counts(sort=True).plot(kind='bar')
+def plot_viewmode(df: pd.DataFrame, title: str):
+    bar_graph_ax = df['view mode'].value_counts(sort=True).plot(kind='bar')
     plt.bar_label(bar_graph_ax.containers[0])
     #bar_graph_ax.set_xticklabels(['Brother', 'Bruh', 'Otto'])
     plt.xticks(rotation=0, ha='center')
-    plt.title(f"View Mode (n={len(data['country'])})")
+    plt.title(f"{title} (n={len(df['country'])})")
     plt.ylabel("Count")
     plt.xlabel("")
     #plt.savefig(OUTPUT_IMAGE_LOCATION + 'ages.png', transparent=IMAGE_TRANSPARENCY, bbox_inches='tight')
     plt.show()
 
-if __name__ == '__main__':
+
+
+
+
+
+def plot_other(df: pd.DataFrame, title: str, column:str):
+
+    d = {}
+    x = []
+    y = []
+    for option in response_options[column]:
+        d[option] = int(0)
+
+    for row in df[column]:
+        d[row] = d[row] + int(1)
+
+    for i in d.items():
+        x.append(i[0])
+        y.append(i[1])
+
+    x, y = list(zip(*sorted(zip(x, y), reverse=False)))
+
+    print(x)
+    print(y)
     
+    plt.bar(range(len(d)), list(d.values()), align='center')
+    plt.xticks(range(len(d)), list(d.keys()))
+    plt.title(f"{title} (n={len(df[column])})")
+    plt.ylabel("Count")
+    plt.xlabel("")
+
+
+    if(SAVE):
+        plt.savefig(OUTPUT_IMAGE_LOCATION + column + '.png', transparent=IMAGE_TRANSPARENCY, bbox_inches='tight')
+    plt.show()
+
+
+def plot_likert(df: pd.DataFrame, title: str, column:str):
+
+    x = response_options[column] # 'unlikely', 'neutral', ....
+    y = np.zeros(5, dtype=int)
+
+    for entry in df[column]:
+        y[entry-1] += 1
+
+    ax = plt.bar(x, y)
+    
+    plt.xticks(rotation=0, ha='center')
+    plt.title(f"{title} (n={len(df[column])})")
+    plt.ylabel("Count")
+    plt.xlabel("")
+    plt.show()
+
+    if(SAVE):
+        plt.savefig(OUTPUT_IMAGE_LOCATION + column + '.png', transparent=IMAGE_TRANSPARENCY, bbox_inches='tight')
+
+
+
+
+if __name__ == '__main__':
+
     source = 'responses.xlsx'
-    column_labels = ['timestamp', 'country', 'local resident', 'age', 'gender', 'view mode', 
-               'overall experience', 'ease of navigation', 'enjoyed presentation', 'load speed',
-               'recommend to friend', 'captures culture', 'use when visiting', 'showcases modernization', 'impactfulness',
-               'favorite story', 'why story', 'favorite place', 'why place',
-               'something you liked', 'something you disliked', 'want to add anything', 'other comments']
+    column_labels = ['timestamp', 'country', 'local resident', 'age', 'gender', 'view mode',                                #options
+               'overall experience', 'ease of navigation', 'enjoyed presentation', 'load speed',                            #likert
+               'recommend to friend', 'captures culture', 'use when visiting', 'showcases modernization', 'impactfulness',  #likert
+               'favorite story', 'why story', 'favorite place', 'why place',                                                #option, short ans
+               'something you liked', 'something you disliked', 'want to add anything', 'other comments']                   #long answer
     
     xls = pd.ExcelFile(source)
 
@@ -225,10 +286,21 @@ if __name__ == '__main__':
     #i = data[((data.country == 'United States'))].index
     #data = data.drop(i)
 
-    setMatplotParams()
-    #plot_country(data)
-    plot_viewmode(data)
+    if(SAVE):
+        try:
+            shutil.rmtree(OUTPUT_IMAGE_LOCATION)
+        except FileNotFoundError:
+            pass
 
+        if not os.path.exists(OUTPUT_IMAGE_LOCATION):
+            os.makedirs(OUTPUT_IMAGE_LOCATION)
+        
+
+    setMatplotParams()
+    plot_other(data, 'Are You a Local?', 'local resident')
+    #plot_country(data)
+    #plot_viewmode(data, "View Mode")
+    #plot_likert(data, "Overall Experience", 'overall experience')
 
     
     
